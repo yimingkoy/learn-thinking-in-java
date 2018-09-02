92d223aea1afd377bba92c2661a4ab47eec5a523
王一鸣 <yimingwa@wangyimings-MacBook-Pro.local>
2018-09-02 16:51:08 +0800

    Chap10-1

---
+++ Inner.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import org.omg.PortableInterceptor.INACTIVE;
+
+public class Inner {
+    class Contents {
+        private  int i = 11;
+        public int value() {return i;}
+    }
+    class Destination {
+        private String label;
+        Destination(String whereTo) {
+            label= whereTo;
+        }
+        String readLable () {return label;}
+    }
+    public void ship(String dest) {
+        Contents c = new Contents();
+        Destination d = new Destination(dest);
+        System.out.println(d.readLable());
+    }
+    public static void main(String[] args) {
+        Inner m_inner = new Inner();
+        m_inner.ship("Tasmania");
+    }
+}
